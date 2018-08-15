85531b9e863ad4109bb69d5cbbc635cec73b673a
Your Name <you@example.com>
2018-08-15 11:25:41 +0800

    优化

--- main.go
+++ main.go
@@ -34,8 +34,8 @@ var (
 )
 
 func main() {
-	version := flag.String("v", "none", "programe's version")
-	appName := flag.String("n", "main", "programe's name")
+	version := flag.String("v", "none", "program's version")
+	appName := flag.String("n", "main", "program's name")
 	flag.Parse()
 	flag.Usage = usage
 	command = flag.Arg(0)
@@ -143,12 +143,12 @@ func usage() {
 	usageStr += "\n"
 	usageStr += "The commands are:\n"
 	usageStr += "\n"
-	usageStr += fmt.Sprintf("	%s [version_code] %s [app_name|main] %s		create %s's programe\n", "-v", "-n", COMMAND_B_DEV, COMMAND_B_DEV)
-	usageStr += fmt.Sprintf("	%s [version_code] %s [app_name|main] %s		create %s's programe\n", "-v", "-n", COMMAND_B_PROD, COMMAND_B_PROD)
-	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s programe\n", "-n", COMMAND_START, COMMAND_START)
-	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s programe\n", "-n", COMMAND_RESTART, COMMAND_RESTART)
-	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s programe\n", "-n", COMMAND_STOP, COMMAND_STOP)
-	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s programe\n", "-n", COMMAND_STATUS, COMMAND_STATUS)
+	usageStr += fmt.Sprintf("	%s [version_code] %s [app_name|main] %s		create %s's program\n", "-v", "-n", COMMAND_B_DEV, COMMAND_B_DEV)
+	usageStr += fmt.Sprintf("	%s [version_code] %s [app_name|main] %s		create %s's program\n", "-v", "-n", COMMAND_B_PROD, COMMAND_B_PROD)
+	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s program\n", "-n", COMMAND_START, COMMAND_START)
+	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s program\n", "-n", COMMAND_RESTART, COMMAND_RESTART)
+	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s program\n", "-n", COMMAND_STOP, COMMAND_STOP)
+	usageStr += fmt.Sprintf("	%s [app_name|main] %s %36s program\n", "-n", COMMAND_STATUS, COMMAND_STATUS)
 	usageStr += fmt.Sprintf("	%s							look up help\n", COMMAND_HELP)
 	usageStr += fmt.Sprintf("	%s							look up dev's version log\n", COMMAND_VER_DEV)
 	usageStr += fmt.Sprintf("	%s						look up prod's version log\n", COMMAND_VER_PROD)
