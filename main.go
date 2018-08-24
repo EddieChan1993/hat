b08ac837836d31a44aa760e9a905319b3e607534
Your Name <you@example.com>
2018-08-24 16:47:12 +0800

    优化

--- main.go
+++ main.go
@@ -106,7 +106,7 @@ func buildProd(v, appName string) {
 }
 
 func nohupApp(appName string) {
-	fmt.Println("please CTRL+D")
+	//fmt.Println("please CTRL+D")
 	isExtraAppName(appName)
 	c := fmt.Sprintf("nohup ./%s &", appName)
 	//fmt.Println(c)
@@ -138,6 +138,17 @@ func isExtraAppName(appName string) {
 		fmt.Println("appName is none")
 		usage()
 		os.Exit(1)
+	}
+	c := "pwd"
+	out, _ := execShellRes(c)
+	out = strings.Replace(out, "\n", "", -1)
+
+	out=fmt.Sprintf("%s/%s",out,appName)
+	_,err :=os.Stat(out)
+	if err != nil {
+		fmt.Println("应用文件不存在")
+		os.Exit(1)
+
 	}
 }
 
@@ -231,6 +242,7 @@ func execShellRes(s string) (r string, err error) {
 	cmd.Stdout = &out
 	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
 	err = cmd.Run()
+
 	return out.String(), err
 }
 
