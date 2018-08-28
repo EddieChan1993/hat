4adf815912f0e501822f99734a530333b409ba17
Your Name <you@example.com>
2018-08-28 11:59:38 +0800

    优化

--- main.go
+++ main.go
@@ -110,7 +110,7 @@ func nohupApp(appName string) {
 	//fmt.Println("please CTRL+D")
 	isExtraAppName(appName)
 	isExtraApp(appName)
-	c := fmt.Sprintf("nohup ./%s > %s 2>&1  &", appName,"nohup.log")
+	c := fmt.Sprintf("nohup ./%s > %s 2>&1  &", appName,"nohup.out")
 	//fmt.Println(c)
 	execShell(c)
 }
