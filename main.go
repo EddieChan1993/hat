cd23e1d9282c39fc3e29893294f821013c0ae2eb
tyria-pc <tyria-pc@example.com>
2018-08-30 12:57:05 +0800

    优化

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os/exec"
 	"os"
 	"time"
-	"io"
-	"bufio"
-	"bytes"
 	"strings"
 	ver "github.com/EddieChan1993/hat/version"
-	"runtime"
 )
 
 const YMD_HIS = "2006-01-02 15:04:05"
@@ -25,7 +20,7 @@ var (
 )
 
 func main() {
-	app := folder()
+	app := ver.Folder()
 	version := flag.String("v", "none", "programe's version")
 	appName := flag.String("n", app, "programe's name")
 	flag.Parse()
@@ -46,6 +41,7 @@ func main() {
 	case ver.COMMAND_RESTART:
 		restartApp(*appName)
 		ver.WriteStart(command)
+		showStatus()
 	case ver.COMMAND_STOP:
 		stopApp(*appName)
 		ver.WriteStop(command)
@@ -68,7 +64,7 @@ func restartApp(appName string) {
 	isExtraApp(appName)
 
 	c := fmt.Sprintf("ps aux | grep \"%s\" | grep -v grep | awk '{print $2}' | xargs -i kill -1 {}", appName)
-	execShell(c)
+	ver.ExecShell(c)
 }
 
 //关闭程序
@@ -77,20 +73,25 @@ func stopApp(appName string) {
 	isExtraApp(appName)
 
 	c := fmt.Sprintf("ps aux | grep \"%s\" | grep -v grep | awk '{print $2}' | xargs -i kill -9 {}", appName)
-	execShell(c)
+	ver.ExecShell(c)
+}
+
+func load(appName, v string) {
+	brand := ver.Branch()
+	commitId := ver.CommitId()
+	ver.Spinner(100*time.Millisecond, fmt.Sprintf("正在编译【%s】程序\n分支:%s,提交ID:%s\n版本号:%s,程序名称:%s", env[ver.COMMAND_B_DEV], brand, commitId, v, appName))
 }
 
 //编译生成开发环境程序
 func buildDev(v, appName string) {
 	versionName := v
-	fmt.Println(versionName)
 	buildCond(v, appName)
 	v = getBuildVer(v, env[ver.COMMAND_B_DEV], ver.COMMAND_B_DEV)
 	v = fmt.Sprintf("v%s", v)
-	go spinner(100*time.Millisecond, fmt.Sprintf("正在编译【%s】程序,版本号:%s,程序名称:%s", env[ver.COMMAND_B_DEV], v, appName))
+	go load(appName,v)
 	versionStr := fmt.Sprintf("-X main._version_=%s", v)
 	c := fmt.Sprintf("go build -ldflags \"%s\" -o %s", versionStr, appName)
-	execShell(c)
+	ver.ExecShell(c)
 	logVersion(versionName, env[ver.COMMAND_B_DEV], ver.COMMAND_B_DEV)
 }
 
@@ -100,26 +101,26 @@ func buildProd(v, appName string) {
 	buildCond(v, appName)
 	v = getBuildVer(v, env[ver.COMMAND_B_PROD], ver.COMMAND_B_PROD)
 	v = fmt.Sprintf("v%s", v)
-	go spinner(100*time.Millisecond, fmt.Sprintf("正在编译【%s】程序,版本号:%s,程序名称:%s", env[ver.COMMAND_B_PROD], v, appName))
+	go load(appName,v)
 	versionStr := fmt.Sprintf("-X main._version_=%s", v)
 	c := fmt.Sprintf("go build -ldflags \"%s\" -tags=prod -o %s", versionStr, appName)
-	execShell(c)
+	ver.ExecShell(c)
 	logVersion(versionName, env[ver.COMMAND_B_PROD], ver.COMMAND_B_PROD)
 }
 
 func nohupApp(appName string) {
-	fmt.Println("please CTRL+Z")
 	isExtraAppName(appName)
 	isExtraApp(appName)
-	c := fmt.Sprintf("nohup ./%s > %s &", appName,"nohup.out")
+	fmt.Println("please CTRL+Z")
+	c := fmt.Sprintf("nohup ./%s > %s &", appName, "nohup.out")
 	//fmt.Println(c)
-	execShell(c)
+	ver.ExecShell(c)
 }
 
 //查看运行状态
 func showStatus() {
 	c := "tail -f nohup.out"
-	execCommand(c)
+	ver.ExecCommand(c)
 }
 
 //编译条件
@@ -148,7 +149,7 @@ func isExtraAppName(appName string) {
 //是否存在执行应用
 func isExtraApp(appName string) {
 	c := "pwd"
-	out, _ := execShellRes(c)
+	out, _ := ver.ExecShellRes(c)
 	out = strings.Replace(out, "\n", "", -1)
 
 	out = fmt.Sprintf("%s/%s", out, appName)
@@ -162,7 +163,7 @@ func isExtraApp(appName string) {
 
 func isExtraMain() {
 	c := "pwd"
-	out, _ := execShellRes(c)
+	out, _ := ver.ExecShellRes(c)
 	out = strings.Replace(out, "\n", "", -1)
 
 	out = fmt.Sprintf("%s/%s", out, "main.go")
@@ -194,107 +195,18 @@ func usage() {
 	fmt.Fprintf(os.Stderr, usageStr)
 }
 
-//获取项目名
-//sys 考虑当前操作系统
-func folder() string {
-	c := "basename $PWD"
-	out, _ := execShellRes(c)
-	out = strings.Replace(out, "\n", "", -1)
-	if runtime.GOOS == `windows` {
-		out = fmt.Sprintf("%s.exe", out)
-	}
-	return out
-}
-
-//编译加载进度
-func spinner(delay time.Duration, title string) {
-	fmt.Printf("%s\n", title)
-	for {
-		for _, r := range `-\|/` {
-			fmt.Printf("\r%c", r)
-			time.Sleep(delay)
-		}
-	}
-}
-
-//需要对shell标准输出的逐行实时进行处理的
-func execCommand(s string) {
-	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
-	cmd := exec.Command("sh", "-c", s)
-	//显示运行的命令
-	//fmt.Println(cmd.Args)
-	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	cmd.Start()
-	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
-	reader := bufio.NewReader(stdout)
-	//实时循环读取输出流中的一行内容
-	for {
-		line, err2 := reader.ReadString('\n')
-		if err2 != nil || io.EOF == err2 {
-			break
-		}
-		fmt.Print(line)
-	}
-	//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
-	cmd.Wait()
-}
-
-//阻塞式的执行外部shell命令的函数,等待执行完毕并返回标准输出
-func execShell(s string) {
-	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
-	cmd := exec.Command("sh", "-c", s)
-	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
-	//var out bytes.Buffer
-	//cmd.Stdout = &out
-	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
-	err := cmd.Run()
-	checkErr(err, "")
-}
-
-//阻塞式的执行外部shell命令的函数,等待执行完毕并返回标准输出，有返回值
-func execShellRes(s string) (r string, err error) {
-	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
-	cmd := exec.Command("sh", "-c", s)
-	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
-	err = cmd.Run()
-
-	return out.String(), err
-}
-
-//异常处理
-func checkErr(err error, out string) {
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	if out == "" {
-		fmt.Println("success")
-		fmt.Println("=======")
-	} else {
-		fmt.Println(string(out))
-	}
-}
-
 //获取编译版本
 func getBuildVer(v, mode, cmd string) string {
 	dateNow := time.Now().Format(YMD_HIS)
 	cmdStr := `git rev-parse --abbrev-ref HEAD`
-	branch, err := execShellRes(cmdStr)
+	branch, err := ver.ExecShellRes(cmdStr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	branch = strings.Replace(branch, "\n", "", -1)
 	cmdStr = `git log --pretty=format:"%h" -1`
-	commitId, err := execShellRes(cmdStr)
+	commitId, err := ver.ExecShellRes(cmdStr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -313,19 +225,8 @@ func getBuildVer(v, mode, cmd string) string {
 //序列化版本
 func logVersion(v, mode, cmd string) string {
 	dateNow := time.Now().Format(YMD_HIS)
-	cmdStr := `git rev-parse --abbrev-ref HEAD`
-	branch, err := execShellRes(cmdStr)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	branch = strings.Replace(branch, "\n", "", -1)
-	cmdStr = `git log --pretty=format:"%h" -1`
-	commitId, err := execShellRes(cmdStr)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	branch := ver.Branch()
+	commitId := ver.CommitId()
 	appV := ver.AppVersion{
 		Model:    mode,
 		Version:  v,
--- version/cmd.go
+++ version/cmd.go
@@ -1,5 +1,17 @@
 package version
 
+import (
+	"fmt"
+	"os"
+	"strings"
+	"os/exec"
+	"bytes"
+	"bufio"
+	"io"
+	"runtime"
+	"time"
+)
+
 const COMMAND_B_DEV = "dev"
 const COMMAND_B_PROD = "prod"
 const COMMAND_START = "start"
@@ -10,3 +22,115 @@ const COMMAND_HELP = "help"
 const COMMAND_VER_DEV = "ver_dev"
 const COMMAND_VER_PROD = "ver_prod"
 const COMMAND_VERS = "vers"
+
+//分支
+func Branch() string {
+	cmdStr := `git rev-parse --abbrev-ref HEAD`
+	branch, err := ExecShellRes(cmdStr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return strings.Replace(branch, "\n", "", -1)
+}
+
+//提交序列号
+func CommitId() string {
+	cmdStr := `git log --pretty=format:"%h" -1`
+	commitId, err := ExecShellRes(cmdStr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return commitId
+}
+
+//阻塞式的执行外部shell命令的函数,等待执行完毕并返回标准输出，有返回值
+func ExecShellRes(s string) (r string, err error) {
+	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
+	cmd := exec.Command("sh", "-c", s)
+	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
+	err = cmd.Run()
+
+	return out.String(), err
+}
+
+//需要对shell标准输出的逐行实时进行处理的
+func ExecCommand(s string) {
+	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
+	cmd := exec.Command("sh", "-c", s)
+	//显示运行的命令
+	//fmt.Println(cmd.Args)
+	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	cmd.Start()
+	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
+	reader := bufio.NewReader(stdout)
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Print(line)
+	}
+	//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
+	cmd.Wait()
+}
+
+//阻塞式的执行外部shell命令的函数,等待执行完毕并返回标准输出
+func ExecShell(s string) {
+	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
+	cmd := exec.Command("sh", "-c", s)
+	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
+	//var out bytes.Buffer
+	//cmd.Stdout = &out
+	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
+	err := cmd.Run()
+	checkErr(err, "")
+}
+
+//异常处理
+func checkErr(err error, out string) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	if out == "" {
+		fmt.Println("success")
+		fmt.Println("=======")
+	} else {
+		fmt.Println(string(out))
+	}
+}
+
+//获取项目名
+//sys 考虑当前操作系统
+func Folder() string {
+	c := "basename $PWD"
+	out, _ := ExecShellRes(c)
+	out = strings.Replace(out, "\n", "", -1)
+	if runtime.GOOS == `windows` {
+		out = fmt.Sprintf("%s.exe", out)
+	}
+	return out
+}
+
+//编译加载进度
+func Spinner(delay time.Duration, title string) {
+	fmt.Printf("%s\n", title)
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
